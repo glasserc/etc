fb6b357932ddbee08550e90d7ed071e67a593932
Ethan Glasser-Camp <ethan@10gen.com>
2008-11-14 18:24:30 -0500

    Revert this -- still playing.

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -95,7 +95,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(compilation-scroll-output (quote first-error))
+ '(compilation-scroll-output t)
  '(display-buffer-reuse-frames t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
