d8c5931828335108bc0507f0bca3bff3ed0ef7ee
Ethan Glasser-Camp <ethan@sundance.nakedbeekey.com>
2008-11-16 22:58:42 -0500

    Re-arrange and reorganize everything.

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -1,45 +1,63 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/packages"))
-(require 'multi-mode)
-(require 'css-mode)
-(require 'show-wspace)
-(require 'java-mode-indent-annotations)
-(require 'elide-head)
+
 
 (iswitchb-mode 1)
 
-(defun jsp-mode () (interactive)
-  (multi-mode 1
-              'html-mode
-              '("<%--" indented-text-mode)
-              '("{" javascript-mode)
-              '("<%@" html-mode)
-              '("<%=" html-mode)
-              '("<%" java-mode)
-              '("%>" html-mode)
-              )
-  )
 
-(setq auto-mode-alist
-      (cons '("\\.js$" . js2-mode)
-            auto-mode-alist))
-
-
-;(setq auto-mode-alist
-;      (cons '("\\.jxp$" . jsp-mode)
-;            auto-mode-alist))
-
-
-(setq auto-mode-alist
-      (cons '("\\.css$" . css-mode)
-            auto-mode-alist))
-
-(require 'git)
+;;; redo
 (require 'redo)
-
-
 (define-key global-map (kbd "M-_") 'redo)
-;(define-key global-map (kbd "M-g") 'goto-line)
+;;; end redo
 
+;;; color theme
+(require 'color-theme)
+(setq color-theme-is-global t)
+(color-theme-charcoal-black)
+;;; end color theme
+
+;;; Emacs 23 font hacking
+(setq default-emacs-font "Terminus-13")
+
+(if (>= emacs-major-version 23)
+    (progn (set-frame-font default-emacs-font)
+           (add-to-list 'default-frame-alist (cons 'font default-emacs-font))))
+;;; end Emacs 23 font hacking
+
+;;; wspace
+; FIXME: take font-lock off in *Completions* buffers
+(require 'show-wspace)
+(add-hook 'font-lock-mode-hook 'show-ws-highlight-tabs)
+(add-hook 'font-lock-mode-hook 'show-ws-highlight-trailing-whitespace)
+
+; FIXME: compute this color based on the current color-theme
+(setq space-color "#562626")
+(set-face-background 'show-ws-tab space-color)
+(set-face-background 'show-ws-trailing-whitespace space-color)
+(set-face-background 'trailing-whitespace space-color)
+;;; end wspace
+
+;;; elide-head
+(require 'elide-head)
+
+(setq elide-head-headers-to-hide
+      (append
+       '(("Copyright (C) 2008 10gen Inc\\." . "If not, see <http")   ; AGPL
+         ("Copyright (C) 2008 10gen Inc\\." . "under the License\\.") ; APL
+         )
+       elide-head-headers-to-hide))
+
+(add-hook 'find-file-hook 'elide-head)
+;;; end elide-head
+
+;;; desktop-mode config
+; I don't expect to ever use it, but it's nice to
+; have.  Save session with desktop-save, then read using desktop-read.
+; desktop-save doesn't overwrite it's previous save.. not sure why or
+; how to fix. Maybe desktop-save-mode?
+(set-default 'desktop-path (list (expand-file-name "~/.emacs.d/")))
+;;; end desktop-mode
+
+;;; generic editing keybindings
 (defun scroll-up-one (arg)
   (interactive "p")
   (scroll-up 1))
@@ -50,45 +68,20 @@
 
 (define-key global-map (kbd "M-<up>") 'scroll-down-one)
 (define-key global-map (kbd "M-<down>") 'scroll-up-one)
+;(define-key global-map (kbd "M-g") 'goto-line)
 
-(require 'color-theme)
-(setq color-theme-is-global t)
-(color-theme-charcoal-black)
+;;; end generic editing keybindings
 
+;;; git -- I never use this any more since git-cola, but hey!
+(require 'git)
+;;; end git
 
-(defun java-mode-untabify ()
-  (save-excursion
-    (goto-char (point-min))
-    (while (re-search-forward "[ \t]+$" nil t)
-      (delete-region (match-beginning 0) (match-end 0)))
-    (goto-char (point-min))
-    (if (search-forward "\t" nil t)
-	(untabify (1- (point)) (point-max))))
-  nil)
-
-(add-hook 'javascript-mode-hook 
-	  '(lambda ()
-	     (make-local-variable 'write-contents-hooks)
-;             (add-hook 'write-contents-hooks 'java-mode-untabify)
-             ))
-
-(add-hook 'jsp-mode-hook 
-	  '(lambda ()
-	     (make-local-variable 'write-contents-hooks)
-;	     (add-hook 'write-contents-hooks 'java-mode-untabify)
-             ))
-
-(add-hook 'find-file-hook 'elide-head)
+;;; java-mode-indent-annotations
+(require 'java-mode-indent-annotations)
 (add-hook 'java-mode-hook 'java-mode-indent-annotations-setup)
-; FIXME: take font-lock off in *Completions* buffers
-(add-hook 'font-lock-mode-hook 'show-ws-highlight-tabs)
-(add-hook 'font-lock-mode-hook 'show-ws-highlight-trailing-whitespace)
+;;; end java-mode-indent-annotations
 
-(setq space-color "#562626")
-(set-face-background 'show-ws-tab space-color)
-(set-face-background 'show-ws-trailing-whitespace space-color)
-(set-face-background 'trailing-whitespace space-color)
-
+;;; customize stuff
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -109,16 +102,66 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+;;; customize stuff
+
+;;; other programming language modes
+(require 'css-mode)
+(setq auto-mode-alist
+      (cons '("\\.css$" . css-mode)
+            auto-mode-alist))
+
+;(setq auto-mode-alist
+;      (cons '("\\.jxp$" . jsp-mode)
+;            auto-mode-alist))
 
 
+(setq auto-mode-alist
+      (cons '("\\.js$" . js2-mode)
+            auto-mode-alist))
 
+
+(defun java-mode-untabify ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "[ \t]+$" nil t)
+      (delete-region (match-beginning 0) (match-end 0)))
+    (goto-char (point-min))
+    (if (search-forward "\t" nil t)
+	(untabify (1- (point)) (point-max))))
+  nil)
+
+(add-hook 'javascript-mode-hook
+	  '(lambda ()
+	     (make-local-variable 'write-contents-hooks)
+;             (add-hook 'write-contents-hooks 'java-mode-untabify)
+             ))
+
+
+(require 'multi-mode)
+(defun jsp-mode () (interactive)
+  (multi-mode 1
+              'html-mode
+              '("<%--" indented-text-mode)
+              '("{" javascript-mode)
+              '("<%@" html-mode)
+              '("<%=" html-mode)
+              '("<%" java-mode)
+              '("%>" html-mode)
+              )
+  )
+
+(add-hook 'jsp-mode-hook
+	  '(lambda ()
+	     (make-local-variable 'write-contents-hooks)
+;	     (add-hook 'write-contents-hooks 'java-mode-untabify)
+             ))
 
 ;;; jxp-mode stuff.
 ;;;
 ;;; This is a symlink to a file that might get updated and on some
 ;;; machines doesn't even exist, so we wrap it in a condition-case (in
 ;;; case it doesn't exist here).
-(condition-case nil
+(condition-case 'jxp-error
     (progn (require 'jxp-mode)
            (require 'mmm-mode)
 
@@ -147,25 +190,9 @@
            (add-to-list 'auto-mode-alist '("\\.jxp\\'" . html-mode))
            ;; mmm-mode conveniently displays jxp code as white-on-white unless the highlight level is set to 0
            (setq mmm-submode-decoration-level 0))
-  (error nil))
+  (error (message "Failed to load jxp mode: %s" jxp-error)))
+; FIXME: fall back on JSP mode?
+;;; end programming language modes
 
 
-(setq elide-head-headers-to-hide
-      (append
-       '(("Copyright (C) 2008 10gen Inc\\." . "If not, see <http")   ; AGPL
-         ("Copyright (C) 2008 10gen Inc\\." . "under the License\\.") ; APL
-         )
-       elide-head-headers-to-hide))
-
-; desktop-mode config -- don't expect to ever use it, but it's nice to
-; have.  Save session with desktop-save, then read using desktop-read.
-; desktop-save doesn't overwrite it's previous save.. not sure why or
-; how to fix. Maybe desktop-save-mode?
-(set-default 'desktop-path (list (expand-file-name "~/.emacs.d/")))
-
-(setq default-emacs-font "Terminus-13")
-
-(if (>= emacs-major-version 23)
-    (progn (set-frame-font default-emacs-font)
-           (add-to-list 'default-frame-alist (cons 'font default-emacs-font))))
 
