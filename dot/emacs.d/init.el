c9886406e093400c645c715c813d0ada224052d1
Ethan Glasser-Camp <ethan@betacantrips.com>
2009-01-26 18:24:47 -0500

    Fewer eye-gougings in rst-mode.

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -44,6 +44,12 @@
 (set-face-background 'show-ws-tab space-color)
 (set-face-background 'show-ws-trailing-whitespace space-color)
 (set-face-background 'trailing-whitespace space-color)
+
+(set-face-background 'rst-level-1-face "#565656")
+(set-face-background 'rst-level-2-face "#4d4d4d")
+(set-face-background 'rst-level-3-face "#464646")
+(set-face-background 'rst-level-4-face "#3d3d3d")
+(set-face-background 'rst-level-5-face "#363636")
 ;;; end wspace
 
 ;;; elide-head
