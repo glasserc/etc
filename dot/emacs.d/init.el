470cf838105dd4055da2af9a0a1bc28dcf1dc1f0
Ethan Glasser-Camp <eglasser@eglasser-mb.ad.hugeinc.com>
2009-05-19 18:03:52 -0400

    More robust against missing pymacs

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -262,18 +262,21 @@
 (global-set-key (kbd "M-/") 'hippie-expand)
 
 ;;; pymacs, ropemacs
-(require 'pymacs)
-(setq python-ropemacs-setup nil)
-(add-hook 'python-mode-hook 'ropemacs-mode)
-(add-hook 'python-mode-hook
-          (lambda ()
-            (if (not python-ropemacs-setup)
-                (progn
-                  (pymacs-load "ropemacs" "rope-")
-                  (setq python-ropemacs-setup t)
-                  (define-key ropemacs-local-keymap
-                    (kbd "M-/") (key-binding (kbd "M-/")))
-                  ))))
+(if (require 'pymacs nil t)
+    (progn
+      (setq python-ropemacs-setup nil)
+      (add-hook 'python-mode-hook 'ropemacs-mode)
+      (add-hook 'python-mode-hook
+                (lambda ()
+                  (if (not python-ropemacs-setup)
+                      (progn
+                        (pymacs-load "ropemacs" "rope-")
+                        (setq python-ropemacs-setup t)
+                        (define-key ropemacs-local-keymap
+                          (kbd "M-/") (key-binding (kbd "M-/")))
+                        )))))
+  (progn ; else
+    (message "WARNING: pymacs missing -- ropemacs absent")))
 
 ;;; git -- I never use this any more since git-cola, but hey!
 ;(require 'git)
