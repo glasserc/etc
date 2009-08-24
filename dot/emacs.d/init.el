7762e0b4082d1e3a464de58b29fa31a0e6d2210b
Ethan Glasser-Camp <ethan@betacantrips.com>
2009-08-24 15:02:32 -0400

    Make these functions interactive-able even before load

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -359,8 +359,8 @@
   ))
 
 ;;; haml-mode
-(autoload 'haml-mode "haml-mode" "Mode for editing HAML code.")
-(autoload 'sass-mode "sass-mode" "Mode for editing SASS code.")
+(autoload 'haml-mode "haml-mode" "Mode for editing HAML code." t)
+(autoload 'sass-mode "sass-mode" "Mode for editing SASS code." t)
 
 (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
