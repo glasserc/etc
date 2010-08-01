70535e2b1e9f3f879d957f7b0ccf443e375c2858
Ethan Glasser-Camp <ethan@joker.(none)>
2010-08-01 07:38:44 -0400

    Move emacs' *custom* into its own file, custom.el

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -140,34 +140,8 @@
 ;;; end java-mode-indent-annotations
 
 ;;; customize stuff
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(compilation-scroll-output t)
- '(display-buffer-reuse-frames t)
- '(ido-max-directory-size 36864)
- '(ido-save-directory-list-file "~/.emacs.d/ido.last")
- '(indent-tabs-mode nil)
- '(inhibit-startup-screen t)
- '(org-agenda-restore-windows-after-quit t)
- '(org-archive-mark-done nil)
- '(org-drawers (quote ("PROPERTIES" "CLOCK" "DETAILS")))
- '(org-refile-targets (quote ((org-agenda-files :level . 1))))
- '(transient-mark-mode t)
- '(uniquify-buffer-name-style (quote reverse) nil (uniquify))
- '(uniquify-separator "/")
- '(visible-bell t)
- '(x-select-enable-clipboard t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(diff-added ((t (:inherit diff-changed :foreground "green"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "red")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 ;;; customize stuff
 
 ;;; customize explicit sets
