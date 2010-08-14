1390c8415f2007f5af2e38ee653770582d4531dd
Ethan Glasser-Camp <ethan@betacantrips.com>
2010-08-14 08:46:24 -0400

    Move python config out of init.el

---
+++ dot/emacs.d/ethan-python.el
@@ -0,0 +1,18 @@
+;;; pymacs, ropemacs
+(if (require 'pymacs nil t)
+    (progn
+      (setq python-ropemacs-setup nil)
+      (add-hook 'python-mode-hook 'ropemacs-mode)
+      (add-hook 'python-mode-hook
+                (lambda ()
+                  (if (not python-ropemacs-setup)
+                      (progn
+                        (pymacs-load "ropemacs" "rope-")
+                        (setq python-ropemacs-setup t)
+                        (define-key ropemacs-local-keymap
+                          (kbd "M-/") (key-binding (kbd "M-/")))
+                        )))))
+  (progn ; else
+    (message "WARNING: pymacs missing -- ropemacs absent")))
+
+(provide 'ethan-python)
--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -79,27 +79,11 @@
 ;;; hippie-expand
 (global-set-key (kbd "M-/") 'hippie-expand)
 
-;;; pymacs, ropemacs
-(if (require 'pymacs nil t)
-    (progn
-      (setq python-ropemacs-setup nil)
-      (add-hook 'python-mode-hook 'ropemacs-mode)
-      (add-hook 'python-mode-hook
-                (lambda ()
-                  (if (not python-ropemacs-setup)
-                      (progn
-                        (pymacs-load "ropemacs" "rope-")
-                        (setq python-ropemacs-setup t)
-                        (define-key ropemacs-local-keymap
-                          (kbd "M-/") (key-binding (kbd "M-/")))
-                        )))))
-  (progn ; else
-    (message "WARNING: pymacs missing -- ropemacs absent")))
-
 ;;; dvc
 ;(add-to-list 'load-path (expand-file-name "~/.emacs.d/packages/dvc-2009-02-05"))
 ;(require 'dvc-autoloads)
 
 (require 'ethan-org)
+(require 'ethan-python)
 (require 'ethan-php)
 (require 'ethan-haml)
