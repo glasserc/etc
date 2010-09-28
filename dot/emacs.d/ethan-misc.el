bb530515bb105caf3b60aaceea8a8a974759a5f8
Ethan Glasser-Camp <ethan@jonah-hex.(none)>
2010-09-28 12:28:38 -0400

    Save Your Scratch

--- dot/emacs.d/ethan-misc.el
+++ dot/emacs.d/ethan-misc.el
@@ -191,4 +191,16 @@
 (when window-system
   (global-unset-key "\C-z"))
 
+;; save scratch: adapted from
+;; http://xxtjaxx.homelinux.net/snippet/SaveYourScratch/, which has
+;; been down for a while
+(defun save-scratch ()
+  (switch-to-buffer (get-buffer "*scratch*"))
+  (write-file "~/.emacs.d/scratch.el" nil)
+  )
+(add-hook 'kill-emacs-hook 'save-scratch)
+(when (file-exists-p "~/.emacs.d/scratch.el")
+    (setq initial-scratch-message (shell-command-to-string "cat ~/.emacs.d/scratch.el")))
+
+
 (provide 'ethan-misc)
