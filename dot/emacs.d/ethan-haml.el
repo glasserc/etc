d28c58da0fd909771203df90f2171f6164d63b9f
Ethan Glasser-Camp <ethan@betacantrips.com>
2011-03-12 14:25:08 -0500

    haml-mode -> el-get

--- dot/emacs.d/ethan-haml.el
+++ dot/emacs.d/ethan-haml.el
@@ -1,5 +1,4 @@
 ;;; haml-mode
-(autoload 'haml-mode "haml-mode" "Mode for editing HAML code." t)
 (autoload 'sass-mode "sass-mode" "Mode for editing SASS code." t)
 
 (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
