8a1d460298877b35f394c6e31712c12e32a2d365
Ethan Glasser-Camp <ethan@betacantrips.com>
2011-03-12 16:46:22 -0500

    Organizing el-get stuff to do elhome first

--- dot/emacs.d/ethan-el-get.el
+++ dot/emacs.d/ethan-el-get.el
@@ -7,14 +7,25 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (require 'el-get)
+
+;; It sucks that we can't put these in organize-paths, but I want them
+;; to work even in the first call to el-get
+(setq bcc-cache-directory (emacs-d "cache/byte-cache"))
+(setq url-cookie-file (emacs-d "cache/url/cookies"))
+
+;; Do this stuff first to init settings and stuff that might be needed
+;; in installing/initing other packages
+(let ((el-get-sources
+       '(( :name elhome
+                 :after elhome-init))))
+  (el-get))
+
 (setq el-get-sources
       '(iedit rst-mode java-mode-indent-annotations haml-mode
                                         ;nxhtml
               (:name yasnippet
                      :after
-                     (lambda () (yas/load-directory (emacs-d "my-snippets"))))
-              (:name elhome
-               :after elhome-init)))
+                     (lambda () (yas/load-directory (emacs-d "my-snippets"))))))
 
 (el-get)
 
--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -45,8 +45,6 @@
   (package-initialize))
 (require 'ethan-elpa)
 
-(setq bcc-cache-directory (emacs-d "cache/byte-cache"))
-(setq url-cookie-file (emacs-d "cache/url/cookies"))
 (require 'ethan-el-get)
 ;;; customize stuff
 (setq custom-file "~/.emacs.d/custom.el")
