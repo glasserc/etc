648266a16874519b30d0713d7d4dcf08f544e714
Ethan Glasser-Camp <ethan@betacantrips.com>
2011-03-13 05:01:25 -0400

    Move haml settings into elhome

--- dot/emacs.d/elhome/startup/70haml.el
+++ dot/emacs.d/elhome/startup/70haml.el
@@ -1,7 +1,4 @@
-;;; haml-mode
 (autoload 'sass-mode "sass-mode" "Mode for editing SASS code." t)
 
 (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
-
-(provide 'ethan-haml)
