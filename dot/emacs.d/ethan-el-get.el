1edfe5c085d365c26b77ef03f47d2f93188ea5cf
Ethan Glasser-Camp <ethan@betacantrips.com>
2011-05-10 02:44:15 -0400

    Nice packages

--- dot/emacs.d/ethan-el-get.el
+++ dot/emacs.d/ethan-el-get.el
@@ -26,6 +26,7 @@
          ;; OK, all the other crap
          paredit
          iedit java-mode-indent-annotations
+         sml-modeline browse-kill-ring
          (:name haml-mode :features nil)  ; autoload's fine, thanks
          (:name whole-line-or-region
                 :after whole-line-or-region-mode)
