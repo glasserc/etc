8bef8f01b82823364294ad6f2d005fc9fd5e0904
Ethan Glasser-Camp <ethan@betacantrips.com>
2011-05-27 14:02:38 -0400

    Provided autoloads that weren't needed, etc.

--- dot/emacs.d/elhome/startup/70haml.el
+++
@@ -1,4 +0,0 @@
-(autoload 'sass-mode "sass-mode" "Mode for editing SASS code." t)
-
-(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
-(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
