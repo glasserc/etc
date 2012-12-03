ccd721a1a73d8c8a7f915b08d9eb84b4133b5ee3
Ethan Glasser-Camp <ethan@betacantrips.com>
2012-12-03 18:00:27 -0500

    Comments

---
+++ dot/conkerorrc/004-gmail.js
@@ -0,0 +1,7 @@
+// FIXME: you can use C-s to search to a link, but opening it doesn't
+// seem obvious..
+
+// Escape unfocuses links, which is occasionally useful.
+// Unfortunately, gmail also uses it to unfocus its own widgets..
+// FIXME: call unfocus and then fall through?
+//define_key(gmail_keymap, "escape", null, $fallthrough);
