45fa10b6bc6af282d639b27cf551f6ea94fcc2a3
Ethan Glasser-Camp <ethan@betacantrips.com>
2012-12-05 21:07:09 -0500

    Notmuch saved searches

--- dot/emacs.d/elhome/settings/notmuch-settings.el
+++ dot/emacs.d/elhome/settings/notmuch-settings.el
@@ -5,3 +5,9 @@
       '(member
         major-mode
         '(offlineimap-mode notmuch-hello-mode)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(notmuch-saved-searches (quote (("inbox" . "tag:inbox") ("unread" . "tag:unread") ("patch-queue-review" . "tag:notmuch::patch and not tag:notmuch::pushed and not tag:notmuch::obsolete and not tag:notmuch::stale and not tag:notmuch::wontfix and (tag:notmuch::moreinfo or tag:notmuch::needs-review)") ("needs-review" . "tag:notmuch::patch and not tag:notmuch::pushed and not tag:notmuch::obsolete and not tag:notmuch::stale and not tag:notmuch::wontfix and not notmuch::moreinfo and tag:notmuch::needs-review") ("notmuch" . "list:notmuch")))))
