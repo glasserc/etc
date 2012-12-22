4ca9643af1f86f8b97b69a5a6a78d64e84c24886
Ethan Glasser-Camp <ethan@betacantrips.com>
2012-12-22 15:33:11 -0500

    emacs: some customizations
    - initsplit-pretty-print so that tracking this stuff is easier. I
      don't remember why I didn't set this earlier (maybe because I
      secretly hate customize).
    
    - css-indent-level = 2. Why is the default 3??
    
    - Reformat a bunch of other stuff, but no changes.

--- dot/emacs.d/custom.el
+++ dot/emacs.d/custom.el
@@ -1,30 +1,42 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(bookmark-save-flag 0)
  '(column-number-mode t)
  '(compilation-scroll-output t)
+ '(css-indent-level 2)
  '(display-buffer-reuse-frames t)
  '(echo-keystrokes 0.1)
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(ediff-window-setup-function
+   (quote ediff-setup-windows-plain))
  '(ido-max-directory-size 36864)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
+ '(initsplit-pretty-print t)
  '(mail-host-address "betacantrips.com")
  '(mouse-yank-at-point t)
  '(sendmail-program "/usr/bin/msmtp")
  '(transient-mark-mode t)
- '(uniquify-buffer-name-style (quote reverse) nil (uniquify))
+ '(uniquify-buffer-name-style
+   (quote reverse)
+   nil
+   (uniquify))
  '(uniquify-separator "/")
  '(visible-bell t)
  '(x-select-enable-clipboard t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(diff-added ((t (:inherit diff-changed :foreground "green"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "red"))))
- '(hl-line ((t (:background "#444444")))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-added
+   ((t
+     (:inherit diff-changed :foreground "green"))))
+ '(diff-removed
+   ((t
+     (:inherit diff-changed :foreground "red"))))
+ '(hl-line
+   ((t
+     (:background "#444444")))))
