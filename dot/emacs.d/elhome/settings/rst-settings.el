c13cb985d5c8f13006219558dfe94103eabfbf88
Ethan Glasser-Camp <ethan@betacantrips.com>
2013-03-23 12:28:45 -0400

    New faces in rst-mode

--- dot/emacs.d/elhome/settings/rst-settings.el
+++ dot/emacs.d/elhome/settings/rst-settings.el
@@ -1,7 +1,7 @@
 ;;; rst face customizations
 ; FIXME: ugly colors
-(set-face-background 'rst-level-1-face "#565656")
-(set-face-background 'rst-level-2-face "#4d4d4d")
-(set-face-background 'rst-level-3-face "#464646")
-(set-face-background 'rst-level-4-face "#3d3d3d")
-(set-face-background 'rst-level-5-face "#363636")
+(set-face-background 'rst-level-1 "#565656")
+(set-face-background 'rst-level-2 "#4d4d4d")
+(set-face-background 'rst-level-3 "#464646")
+(set-face-background 'rst-level-4 "#3d3d3d")
+(set-face-background 'rst-level-5 "#363636")
