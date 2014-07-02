e7c1b051b3d5b887de333b8585dfd5a98c3fce6e
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-07-01 20:29:16 -0400

    emacs.d: documentation

--- dot/emacs.d/el-get-init-files/init-delight.el
+++ dot/emacs.d/el-get-init-files/init-delight.el
@@ -1,4 +1,7 @@
 ;; Hide some modeline lighters.
+;; Some of these can be defined specially -- see for example
+;; flyspell-mode-line-string and undo-tree-mode-lighter -- but keep
+;; them all here for consistency.
 (eval-after-load 'delight
   '(delight
     '((emacs-lisp-mode "EL" :major)
