02897d2673a4abaeee49d995edeab28252a70341
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-07-01 20:27:21 -0400

    emacs.d: more modeline lighters
    Also, rearrange to match order of display.

--- dot/emacs.d/el-get-init-files/init-delight.el
+++ dot/emacs.d/el-get-init-files/init-delight.el
@@ -2,11 +2,13 @@
 (eval-after-load 'delight
   '(delight
     '((emacs-lisp-mode "EL" :major)
-      (rainbow-mode)
       (paredit-mode " ()" paredit)
       (eldoc-mode "" eldoc)
       (magit-auto-revert-mode nil magit)
       (js-mode "JS" :major)
+      (flyspell-mode " ⚜" flyspell) ; looks kinda like a fly
       (yas-minor-mode " ¥" yasnippet)
       (undo-tree-mode "⎌" undo-tree)
+      (rainbow-mode)
+      (visual-line-mode nil simple)
       (whole-line-or-region-mode nil whole-line-or-region))))
