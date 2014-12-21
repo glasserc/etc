2e9d6ecf0cd77f6277efa04d6d683f9460d64477
Ethan Glasser-Camp <ethan@betacantrips.com>
2014-12-20 19:46:14 -0500

    emacs: haskell indentation stuff

--- dot/emacs.d/elhome/settings/haskell-settings.el
+++ dot/emacs.d/elhome/settings/haskell-settings.el
@@ -3,6 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(haskell-compile-cabal-build-command "cd %s && cabal build --ghc-option=-ferror-spans && hlint src/")
+ '(haskell-indent-spaces 4)
+ '(haskell-indentation-cycle-warn nil)
+ '(haskell-indentation-ifte-offset 4)
+ '(haskell-indentation-layout-offset 4)
+ '(haskell-indentation-left-offset 4)
+ '(haskell-indentation-starter-offset 4)
+ '(haskell-indentation-where-post-offset 2)
+ '(haskell-indentation-where-pre-offset 2)
  '(haskell-mode-hook
    (quote
     (imenu-add-menubar-index turn-on-eldoc-mode turn-on-haskell-decl-scan turn-on-haskell-doc turn-on-haskell-indentation subword-mode))))
