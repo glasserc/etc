a6b96429030a321ccf02184520a02810f17b6f5b
Ethan Glasser-Camp <ethan@betacantrips.com>
2015-11-12 20:51:13 -0500

    disable ffap
    I find it more annoying than useful.

--- dot/emacs.d/elhome/site-lisp/ethan-misc.el
+++ dot/emacs.d/elhome/site-lisp/ethan-misc.el
@@ -22,8 +22,7 @@
 
 (require 'ido)
 (ido-mode 1)
-(setq ido-enable-flex-matching t
-      ido-use-filename-at-point 'guess)
+(setq ido-enable-flex-matching t)
 
 ;; Ido: don't ignore project.git, but ignore .git itself.
 (let ((vcs-extensions '(".svn/" ".hg/" ".git/" ".bzr/")))
