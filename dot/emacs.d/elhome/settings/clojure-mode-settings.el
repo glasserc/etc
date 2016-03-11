159ddd94472c36e89b004590679ddf426fc19f1f
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-11 11:51:26 -0500

    Blow away configuration that I don't use any more

--- dot/emacs.d/elhome/settings/clojure-mode-settings.el
+++
@@ -1,5 +0,0 @@
-(font-lock-add-keywords
-    'clojure-mode `(("(\\(fn\\>\\)"
-                     (0 (progn (compose-region (match-beginning 1)
-                                               (match-end 1) "ƒ")
-                               nil)))))
