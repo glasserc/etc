f365dca3c50b8d307d7deb73517e619e1f2789be
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-11 11:53:36 -0500

    I've never written clojure

--- dot/emacs.d/elhome/settings/lisp-mode-settings.el
+++ dot/emacs.d/elhome/settings/lisp-mode-settings.el
@@ -37,7 +37,3 @@
 
 (eval-after-load 'find-file-in-project
   '(add-to-list 'ffip-patterns "*.clj"))
-
-(defun clojure-project ()
-  (interactive)
-  (message "Deprecated in favour of M-x swank-clojure-project. Install swank-clojure from ELPA."))
