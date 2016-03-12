2636bbb2d23348430c2cabda461b25348abbd0ad
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-12 15:47:02 -0500

    we don't need to look in bootstrap any more

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -12,7 +12,6 @@
   (concat user-emacs-directory filename))
 
 (add-to-list 'load-path (emacs-d "lisp"))
-(add-to-list 'load-path (emacs-d "bootstrap"))
 (add-to-list 'load-path (expand-file-name  "~/local/share/emacs/site-lisp/"))
 (add-to-list 'load-path (expand-file-name  "~/.local/share/emacs/site-lisp/"))
 
