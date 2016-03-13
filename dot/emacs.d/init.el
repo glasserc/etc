12377e2f56a7be1c341ca2a14919688cfe82769b
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-13 18:53:58 -0400

    move remaining elhome stuff to lisp

--- dot/emacs.d/init.el
+++ dot/emacs.d/init.el
@@ -36,6 +36,7 @@
 (require 'ethan-programming)
 (require 'ethan-communication)
 (require 'ethan-bindings)
+(require 'ethan-enabled-commands)   ;; FIXME: merge with bindings?
 (require 'ethan-java)
 (require 'ethan-lisp)
 (require 'ethan-rst)
