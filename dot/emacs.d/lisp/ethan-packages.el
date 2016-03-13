db00d8d1310cdaa66e99a416b101570853904996
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-13 18:00:40 -0400

    move cedet stuff into ethan-packages
    At least this gets it all in one place, rather than being ignored
    piecemeal.

--- dot/emacs.d/lisp/ethan-packages.el
+++ dot/emacs.d/lisp/ethan-packages.el
@@ -385,10 +385,18 @@
 (load-theme 'lush t)
 
 ;; CEDET/semantic/malabar stuff
+;; I don't use CEDET, but these were settings I found useful
 ;(require 'semantic)
 ;(require 'cedet)
 ;(load "semantic/loaddefs.el")
 ;(semantic-mode 1)
+;(global-ede-mode t)
+; (semantic-load-enable-excessive-code-helpers)
+; (require 'semantic-ia)
+; (setq semanticdb-default-save-directory (emacs-d "cache/semanticdb"))
+;; Some kind of weird bug with exuberent-ctags in python-mode?
+;;(semantic-load-enable-primary-exuberent-ctags-support)
+;;(require 'semantic-gcc)
 
 (use-package rich-minority
   :config
