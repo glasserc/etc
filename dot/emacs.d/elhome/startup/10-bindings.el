14f94af2230b0e2e28dedff54cf23f0295869f8c
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-13 16:26:26 -0400

    we don't really need occur as much with swiper

--- dot/emacs.d/elhome/startup/10-bindings.el
+++ dot/emacs.d/elhome/startup/10-bindings.el
@@ -46,11 +46,6 @@
 ;; For debugging Emacs modes
 (global-set-key (kbd "C-c p") 'message-point)
 
-;; Activate occur easily inside isearch
-(define-key isearch-mode-map (kbd "C-o")
-  (lambda () (interactive)
-    (let ((case-fold-search isearch-case-fold-search))
-      (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
 
 ;; Personal customization
 ; At present, emacs does not define bindings for these keys,
