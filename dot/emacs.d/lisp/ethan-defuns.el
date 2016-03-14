242e2b746dfbf1aea09e6a74e1ebffbbae48bf0e
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-13 22:36:50 -0400

    gather view-url stuff in navigation

--- dot/emacs.d/lisp/ethan-defuns.el
+++ dot/emacs.d/lisp/ethan-defuns.el
@@ -2,21 +2,6 @@
 (require 'thingatpt)
 (require 'imenu)
 
-;; Network
-
-(defun view-url ()
-  "Open a new buffer containing the contents of URL."
-  (interactive)
-  (let* ((default (thing-at-point-url-at-point))
-         (url (read-from-minibuffer "URL: " default)))
-    (switch-to-buffer (url-retrieve-synchronously url))
-    (rename-buffer url t)
-    ;; TODO: switch to nxml/nxhtml mode
-    (cond ((search-forward "<?xml" nil t) (xml-mode))
-          ((search-forward "<html" nil t) (html-mode)))))
-
-;; Other
-
 (defun recompile-init ()
   "Byte-compile all your dotfiles again."
   (interactive)
--- dot/emacs.d/lisp/ethan-navigation.el
+++ dot/emacs.d/lisp/ethan-navigation.el
@@ -28,6 +28,17 @@
 (windmove-default-keybindings) ;; Shift+direction
 
 ;; Fetch the contents at a URL, display it raw.
+(defun view-url ()
+  "Open a new buffer containing the contents of URL."
+  (interactive)
+  (let* ((default (thing-at-point-url-at-point))
+         (url (read-from-minibuffer "URL: " default)))
+    (switch-to-buffer (url-retrieve-synchronously url))
+    (rename-buffer url t)
+    ;; TODO: switch to nxml/nxhtml mode
+    (cond ((search-forward "<?xml" nil t) (xml-mode))
+          ((search-forward "<html" nil t) (html-mode)))))
+
 (bind-key "C-x C-h" 'view-url)
 
 ;; Personal customization
