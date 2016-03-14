51902220a6b5a137a71d5a560bab0e1407f622eb
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-03-13 22:07:50 -0400

    use bind-key macro everywhere
    Don't collect in one bind-keys yet because we may still want to move
    them around.

--- dot/emacs.d/lisp/ethan-bindings.el
+++ dot/emacs.d/lisp/ethan-bindings.el
@@ -1,20 +1,20 @@
 ;; Taken from the Emacs Starter Kit
-(global-set-key (kbd "C-x \\") 'align-regexp)
+(bind-key "C-x \\" 'align-regexp)
 
 ;; Start eshell or switch to it if it's active.
-(global-set-key (kbd "C-x m") 'eshell)
+(bind-key "C-x m" 'eshell)
 
 ;; Start a new eshell even if one is active.
-(global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t)))
+(bind-key "C-x M" (lambda () (interactive) (eshell t)))
 
 ;; Start a regular shell if you prefer that.
-(global-set-key (kbd "C-x M-m") 'shell)
+(bind-key "C-x M-m" 'shell)
 
 ;; If you want to be able to M-x without meta (phones, etc)
-(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(bind-key "C-x C-m" 'execute-extended-command)
 
 ;; Help should search more than just commands.
-(global-set-key [remap apropos-command] 'apropos)
+(bind-key [remap apropos-command] 'apropos)
 
 ;; In theory these are reserved for major modes, but I like the python-mode
 ;; bindings (python-indent-shift-left and
@@ -23,7 +23,7 @@
 ;; python-mode. increase-left-margin and decrease-left-margin are OK
 ;; substitutes in other contexts, so make them global. They reflow the
 ;; text, so let python-mode override them.
-(global-set-key [?\C-c ?>] 'increase-left-margin)
-(global-set-key [?\C-c ?<] 'decrease-left-margin)
+(bind-key [?\C-c ?>] 'increase-left-margin)
+(bind-key [?\C-c ?<] 'decrease-left-margin)
 
 (provide 'ethan-bindings)
