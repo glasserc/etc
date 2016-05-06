aac724cdecf89c8a4cff417938676880e7ea5385
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-05-06 18:03:43 -0400

    take hostname out of right prompt

--- dot/config/fish/functions/fish_right_prompt.fish
+++ dot/config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,7 @@
 function fish_right_prompt
     # We don't need this; we're pretty much always ourselves
     #echo -n -s "$USER" @
-    echo -n "$__fish_prompt_hostname"
+    # We don't need this; it's already in the title bar (see
+    # fish_title.fish)
+    #echo -n $__fish_prompt_hostname
 end
