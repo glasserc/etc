4f999c7a9e942fc69a3879fd027281c6451705f8
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-05-07 13:03:19 -0400

    move cwd to right prompt
    I'm not quite ready to get rid of it entirely.

--- dot/config/fish/functions/fish_prompt.fish
+++ dot/config/fish/functions/fish_prompt.fish
@@ -13,22 +13,14 @@ function fish_prompt --description 'Write out the prompt'
     set -l suffix
     switch $USER
         case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
             set suffix '#'
         case '*'
-            set color_cwd $fish_color_cwd
             set suffix '>'
     end
 
     if test $last_status -ne 0
         echo -n -s (set_color $fish_color_error) $last_status (set_color normal) ' '
     end
-
-    echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal)
 
     # Just show operation status in the prompt.
     # We don't want the whole git prompt, but there's some useful
@@ -49,7 +41,7 @@ function fish_prompt --description 'Write out the prompt'
                 case '*'; echo U
             end)
 
-            echo -n -s ' ' (set_color -o $fish_color_error) $operation (set_color normal)
+            echo -n -s (set_color -o $fish_color_error) $operation (set_color normal)
         end
     end
 
--- dot/config/fish/functions/fish_right_prompt.fish
+++ dot/config/fish/functions/fish_right_prompt.fish
@@ -5,9 +5,24 @@ function fish_right_prompt
     # fish_title.fish)
     #echo -n $__fish_prompt_hostname
 
+    set -l color_cwd
+    set -l suffix
+    switch $USER
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+        case '*'
+            set color_cwd $fish_color_cwd
+    end
+
+    echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal)
+
     __show_only_git_branch
 end
 
 function __show_only_git_branch
-    __fish_git_prompt "%s"
+    __fish_git_prompt " %s"
 end
