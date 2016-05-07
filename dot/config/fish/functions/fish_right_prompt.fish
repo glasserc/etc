b660d22df4854140ea14a5b37f75345650c9140a
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-05-07 13:17:05 -0400

    add CMD_DURATION warning

--- dot/config/fish/functions/fish_right_prompt.fish
+++ dot/config/fish/functions/fish_right_prompt.fish
@@ -20,6 +20,10 @@ function fish_right_prompt
 
     echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal)
 
+    if test $CMD_DURATION -gt 5000
+        echo -n -s ' ' $CMD_DURATION 'ms'
+    end
+
     __show_only_git_branch
 end
 
