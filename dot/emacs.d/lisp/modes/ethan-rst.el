3e5f805dbcb4f13f51ec888afb4d074d47468bca
Ethan Glasser-Camp <ethan@betacantrips.com>
2016-05-20 14:16:20 -0400

    modify colors to work better with feng-shui theme

--- dot/emacs.d/lisp/modes/ethan-rst.el
+++ dot/emacs.d/lisp/modes/ethan-rst.el
@@ -2,10 +2,19 @@
 ; FIXME: ugly colors
 (use-package rst :defer
   :config
-  (set-face-background 'rst-level-1 "#565656")
-  (set-face-background 'rst-level-2 "#4d4d4d")
-  (set-face-background 'rst-level-3 "#464646")
-  (set-face-background 'rst-level-4 "#3d3d3d")
-  (set-face-background 'rst-level-5 "#363636"))
+  ;; FIXME: these are based on whatever theme I happened to be running
+  ;; last and they should really be based on some kind of automatic
+  ;; thing, maybe using defface as in
+  ;; http://emacs.stackexchange.com/questions/9600/how-can-i-override-a-pre-defined-face-for-light-and-dark-backgrounds
+  ;; or maybe something even more subtle. But at present there's no
+  ;; convenient way to do this. ethan-wspace does this by using a
+  ;; window-configuration-change-hook which is probably also wrong. It
+  ;; would be cool if there was an emacs library that let you register
+  ;; faces that wer based on the current theme background.
+  (set-face-background 'rst-level-1 "#c6c6c6")
+  (set-face-background 'rst-level-2 "#dddddd")
+  (set-face-background 'rst-level-3 "#d6d6d6")
+  (set-face-background 'rst-level-4 "#ededed")
+  (set-face-background 'rst-level-5 "#e6e6e6"))
 
 (provide 'ethan-rst)
