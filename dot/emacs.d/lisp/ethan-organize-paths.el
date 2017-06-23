26853fc9dce54b8bd76df3680296cf58376d98fd
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-06-22 20:38:06 -0400

    url doesn't seem to save cookies any more?

--- dot/emacs.d/lisp/ethan-organize-paths.el
+++ dot/emacs.d/lisp/ethan-organize-paths.el
@@ -2,7 +2,6 @@
 ;; FIXME: Move all of these to no-littering.
 (let ((tmp "~/.emacs.d/cache/"))
   (setq
-   url-cookie-file (concat tmp "url/cookies")
    srecode-map-save-file (emacs-d "cache/srecode-map.el")))
 
 (provide 'ethan-organize-paths)
