2e5d9b2ebd0a2d2526a6b64d0e0262bcceb51a77
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-08-28 12:16:55 -0400

    More documentation about the nix-shell config I added

--- dot/config/fish/functions/fish_right_prompt.fish
+++ dot/config/fish/functions/fish_right_prompt.fish
@@ -28,6 +28,12 @@ function __show_only_git_branch
     __fish_git_prompt " %s"
 end
 
+# This is somewhat incomplete -- ideally we'd show information about
+# what kind of shell we were in -- but there appear to be upstream
+# political obstacles to getting better nix-shell support for non-bash
+# shells. See https://github.com/NixOS/nix/issues/498 for an overview
+# and https://github.com/chisui/zsh-nix-shell for an example of what a
+# zsh user does by way of contrast.
 function show_nix_status
     if set -q IN_NIX_SHELL
         set -l letter N
