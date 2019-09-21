b7eb4106f8feed0d2a19d0cb688163ad248327ab
Ethan Glasser-Camp <ethan@betacantrips.com>
2019-09-21 11:36:55 -0400

    Use login keyring for auth

--- dot/emacs.d/custom.el
+++ dot/emacs.d/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-sources
+   (quote
+    ("secrets:Login" "~/.authinfo" "~/.authinfo.gpg" "~/.netrc")))
  '(bookmark-save-flag 0)
  '(column-number-mode t)
  '(compilation-scroll-output t)
