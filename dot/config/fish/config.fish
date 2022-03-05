435028fc5ea5cbcca63ab4b15ffb13cee978341e
Ethan Glasser-Camp <ethan@betacantrips.com>
2022-03-05 17:09:44 -0500

    Successfully debugged getting asdf/direnv working

--- dot/config/fish/config.fish
+++ dot/config/fish/config.fish
@@ -22,12 +22,21 @@ end
 # https://github.com/asdf-vm/asdf/issues/687 (and linked issues).
 # asdf uses ASDF_DATA_DIR to figure out where it should be installed.
 set -x ASDF_DATA_DIR ~/.config/asdf
+# We might need this too because of asdf-direnv?
+# See https://github.com/asdf-community/asdf-direnv/issues/102.
+set -x ASDF_DIR ~/.config/asdf
 if test -f $ASDF_DATA_DIR
     # Using asdf only through direnv
     # See https://github.com/asdf-community/asdf-direnv, under "Pro-Tips".
     source $ASDF_DATA_DIR/lib/asdf.fish
 end
 add_path $ASDF_DATA_DIR/bin
+# Be sure to install the direnv plugin.
 
 # direnv installed system-wide using apt rather than managed using asdf
 #eval (asdf exec direnv hook fish)
+# You also need to ensure that asdf uses it:
+#    asdf global direnv system
+# You might need to install at least one version of direnv too:
+#    asdf install direnv latest
+# See https://github.com/asdf-vm/asdf/issues/1130 and https://github.com/asdf-community/asdf-direnv/issues/103.
