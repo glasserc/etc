da5ce6c0199baeea0c3db12d83f6bd67ab7e9115
Ethan Glasser-Camp <ethan@betacantrips.com>
2022-03-05 15:12:13 -0500

    Borrow add_path function from the Internet and use it for local bin
    This is how you might install utilities using cargo or pip, for example.

--- dot/config/fish/config.fish
+++ dot/config/fish/config.fish
@@ -2,6 +2,16 @@
 set -g __fish_git_prompt_show_informative_status true
 set -g __fish_git_prompt_showcolorhints true
 
+# Take this out in fish 3.2.0
+function add_path --description "Utility to add stuff to fish_user_paths idempotently."
+    set -l path $argv[1]
+    if [ -e $path ]
+        contains $path $fish_user_paths; or set -Ua fish_user_paths $path
+    end
+end
+
+add_path ~/.local/bin
+
 if which direnv >/dev/null
     eval (direnv hook fish)
 end
