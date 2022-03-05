07be2f3dd8fbc9a2d55df92be07f4bf7f355a63c
Ethan Glasser-Camp <ethan@betacantrips.com>
2022-03-05 15:23:14 -0500

    Reformat custom.el
    I feel like this has been outstanding for years, so let's just bow to
    the inevitable.

--- dot/emacs.d/custom.el
+++ dot/emacs.d/custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-sources
-   (quote
-    ("secrets:Login" "~/.authinfo" "~/.authinfo.gpg" "~/.netrc")))
+   '("secrets:Login" "~/.authinfo" "~/.authinfo.gpg" "~/.netrc"))
  '(bookmark-save-flag 0)
  '(column-number-mode t)
  '(compilation-scroll-output t)
@@ -13,33 +12,31 @@
  '(display-buffer-reuse-frames t)
  '(echo-keystrokes 0.1)
  '(ediff-diff-options "-w")
- '(ediff-split-window-function (quote split-window-horizontally))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(eshell-cmpl-autolist t)
  '(eshell-cmpl-cycle-completions nil)
  '(imenu-auto-rescan-maxout 150000)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(initsplit-pretty-print t)
- '(mail-envelope-from (quote header))
+ '(mail-envelope-from 'header)
  '(mail-host-address "betacantrips.com")
  '(mail-specify-envelope-from t)
  '(mode-require-final-newline nil)
  '(safe-local-variable-values
-   (quote
-    ((org-time-clocksum-format :hours "%d" :require-hours t :minutes ":%02d" :require-minutes t)
+   '((org-time-clocksum-format :hours "%d" :require-hours t :minutes ":%02d" :require-minutes t)
      (require-final-newline)
-     (nxml-child-indent . 4))))
+     (nxml-child-indent . 4)))
  '(select-enable-clipboard t)
- '(send-mail-function (quote sendmail-send-it))
+ '(send-mail-function 'sendmail-send-it)
  '(sendmail-program "/usr/bin/msmtp")
  '(sql-postgres-login-params
-   (quote
-    ((user :default "ethan")
+   '((user :default "ethan")
      (database :default "ethan")
-     port server)))
+     port server))
  '(transient-mark-mode t)
- '(uniquify-buffer-name-style (quote reverse) nil (uniquify))
+ '(uniquify-buffer-name-style 'reverse nil (uniquify))
  '(uniquify-separator "/")
  '(visible-bell t))
 (custom-set-faces
