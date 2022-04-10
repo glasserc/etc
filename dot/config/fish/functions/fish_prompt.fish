41f5de1a25ff9403ea47da470fc9f837f48953cd
Ethan Glasser-Camp <ethan@betacantrips.com>
2022-04-10 13:11:59 -0400

    Fish is deprecating the caret for redirecting stderr
    See https://github.com/fish-shell/fish-shell/issues/4394 for details.

--- dot/config/fish/functions/fish_prompt.fish
+++ dot/config/fish/functions/fish_prompt.fish
@@ -26,7 +26,7 @@ function fish_prompt --description 'Write out the prompt'
     # Just show operation status in the prompt.
     # We don't want the whole git prompt, but there's some useful
     # functions defined that we want to use.
-    set -l repo_info (command git rev-parse --git-dir ^/dev/null)
+    set -l repo_info (command git rev-parse --git-dir 2>/dev/null)
     if test -n "$repo_info"
         set -l operation (__ethan_git_prompt_short_operation $repo_info)
         if test -n $operation
