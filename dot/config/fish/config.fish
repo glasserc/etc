5f55b59923aebeadda76979a5606a15e87bcfd67
Ethan Glasser-Camp <ethan@betacantrips.com>
2023-02-14 21:08:21 -0500

    This seems like it could be useful

--- dot/config/fish/config.fish
+++ dot/config/fish/config.fish
@@ -1,6 +1,7 @@
 # This seems to do something strange when I set them as part of a prompt.
 set -g __fish_git_prompt_show_informative_status true
 set -g __fish_git_prompt_showcolorhints true
+set -gx EDITOR emacsclient
 
 # Take this out in fish 3.2.0
 function add_path --description "Utility to add stuff to fish_user_paths idempotently."
